998ad666ec6de60c2b5fe1f6078fb2286bca253b
JosephBK <bazaare.joseph@gmail.com>
2016-07-23 16:41:00 +0200

    Add user approval and confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,14 @@ class User < ActiveRecord::Base
   acts_as_authentic do |c|
      c.crypto_provider = Authlogic::CryptoProviders::SCrypt
   end
-
+  after_create :activate_user
+    def activate_user
+      #if a user is successfully created set the active field to true
+      self.active = true
+      self.approved = true
+      self.confirmed = true
+      self.save
+    end
   # def valid_password?(entered_password) 
   #   #TODO Implement password checking  
   #   if entered_password == password 
