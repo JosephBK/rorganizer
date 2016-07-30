c68dd94b8cea00888a55ae2cc846a5bc0a05d359
JosephBK <bazaare.joseph@gmail.com>
2016-07-30 16:38:51 +0200

    Modified README.rdoc and database.yml file

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160723130402) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "courses", force: :cascade do |t|
     t.string   "name"
     t.string   "description"
@@ -30,8 +33,8 @@ ActiveRecord::Schema.define(version: 20160723130402) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "user_sessions", ["session_id"], name: "index_user_sessions_on_session_id"
-  add_index "user_sessions", ["updated_at"], name: "index_user_sessions_on_updated_at"
+  add_index "user_sessions", ["session_id"], name: "index_user_sessions_on_session_id", using: :btree
+  add_index "user_sessions", ["updated_at"], name: "index_user_sessions_on_updated_at", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
@@ -54,6 +57,6 @@ ActiveRecord::Schema.define(version: 20160723130402) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
